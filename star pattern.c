e6cf55461a0f7546fda1c6a44cc91b243cedfd77
Name123839 <24110cn141@glbitm.ac.in>
2025-09-10 23:30:06 +0530

    Update star pattern.c

--- star pattern.c
+++ star pattern.c
@@ -1,23 +1,26 @@
-// #include <stdio.h>
-// int main()
-// {
-//   int i,j;
-//   for(i=1;i<=5;i++)
-//   {
-//     for(j=1;j<=5;j++)
-//     {
-//     printf("*");
-//     }
-//     printf("\n");
-//   }
-//   return 0;
-// }
+// SIMPLE PATTERN
+#include <stdio.h>
+ int main()
+ {
+  int i,j;
+   for(i=1;i<=5;i++)
+   {
+     for(j=1;j<=5;j++)
+     {
+    printf("*");
+    }
+     printf("\n");
+   }
+  return 0;
+ }
 
 // *****
 // *****
 // *****
 // *****
 // *****
+
+HOLLOW PATTERN
 # include <stdio.h>
 int main()
 {
@@ -37,8 +40,8 @@ int main()
       return 0;
   }
       
-*****
-*   *
-*   *
-*   *
-*****
+// *****
+// *   *
+// *   *
+// *   *
+// ***** 
